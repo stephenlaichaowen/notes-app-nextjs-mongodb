9acc1cbc9124dc1998acd9f7c1998bad01780c43
stephenlaichaowen <stephenlaitest@gmail.com>
2021-02-16 22:44:06 +0800

    'commit'

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   env: {
     MONGO_URI: "mongodb+srv://stephenlai2015:NYNuJureSyoKVGge@cluster0.8ql1n.mongodb.net/note-app?retryWrites=true&w=majority"
+  },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    return config
   }
 }
